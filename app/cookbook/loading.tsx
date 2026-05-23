f7998e27f2c0fbff9b673ffc8c4e959a511e2886
Carther Theogene <mrrCarter@users.noreply.github.com>
2026-05-22 20:19:02 -0400

    feat: add route loading skeletons
    Signed-off-by: Carther Theogene <mrrCarter@users.noreply.github.com>

---
+++ app/cookbook/loading.tsx
@@ -0,0 +1,12 @@
+/**
+ * SPDX-License-Identifier: GPL-3.0-only
+ * Copyright (C) 2026 Cursor Boston
+ * This file is part of Cursor Boston, licensed under GPL-3.0.
+ * See LICENSE file for details.
+ */
+
+import { CookbookPageSkeleton } from "@/components/skeletons/CookbookPageSkeleton";
+
+export default function Loading() {
+  return <CookbookPageSkeleton />;
+}
